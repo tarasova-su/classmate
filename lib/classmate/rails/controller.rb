92c767747d293672283cd80ef3b067ad6151790d
ahmadeeva-su <ahmadeeva.su@gmail.com>
2016-06-30 10:09:55 +0500

    Use all params, except coming from Rails, to calculate signatures

--- lib/classmate/rails/controller.rb
+++ lib/classmate/rails/controller.rb
@@ -27,6 +27,7 @@ module Classmate
       CLASSMATE_PARAM_NAMES = %w{logged_user_id api_server application_key session_key session_secret_key authorized apiconnection refplace referer
         auth_sig sig custom_args ip_geo_location new_sig payment_promo_active}
       DEBUG_PARAMS = %w{first_start clientLog web_server}
+      RAILS_PARAMS = %w{controller action}
 
       # Accessor to current application config. Override it in your controller
       # if you need multi-application support or per-request configuration selection.
@@ -46,13 +47,13 @@ module Classmate
 
       # params coming directly from Odnoklassniki
       def classmate_params
-        params.slice(*(CLASSMATE_PARAM_NAMES + DEBUG_PARAMS))
+        params.except(*RAILS_PARAMS)
       end
 
       # encrypted classmate params
       def cm_signed_params
-        if params['session_key'].present?
-          encrypt(params)
+        if classmate_params['session_key'].present?
+          encrypt(classmate_params)
         else
           request.env["HTTP_SIGNED_PARAMS"] || request.params['signed_params'] || flash[:signed_params]
         end
@@ -60,8 +61,8 @@ module Classmate
 
       # FIXME params to initialize JS API - might be better to store in cookies
       def init_js_params
-        if params['session_key'].present?
-          params.slice('api_server', 'apiconnection')
+        if classmate_params['session_key'].present?
+          classmate_params.slice('api_server', 'apiconnection')
         else
           decrypt(cm_signed_params).try(:slice, 'api_server', 'apiconnection')
         end
@@ -69,13 +70,13 @@ module Classmate
 
       # Did the request come from canvas app
       def cm_canvas?
-        params['session_key'].present? || request.env['HTTP_SIGNED_PARAMS'].present? || flash[:signed_params].present?
+        classmate_params['session_key'].present? || request.env['HTTP_SIGNED_PARAMS'].present? || flash[:signed_params].present?
       end
 
       private
 
         def fetch_current_classmate_user
-          Classmate::User.from_classmate_params(classmate, params['session_key'].present? ? params : cm_signed_params)
+          Classmate::User.from_classmate_params(classmate, classmate_params['session_key'].present? ? classmate_params : cm_signed_params)
         end
 
         def encrypt(params)
